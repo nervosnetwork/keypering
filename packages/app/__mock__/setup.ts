7d3db343bfc5db5d196b83df9d050a6db6477807
Keith <keithwhisper@gmail.com>
2020-08-13 23:40:31 +0800

    test: add tests of setting

--- packages/app/__mock__/setup.ts
+++ packages/app/__mock__/setup.ts
@@ -4,22 +4,25 @@ import fsMock from 'fs'
 import { mockWallet, mockSetting, mockSettingMeta, mockAuth, mockTxList } from '.'
 
 jest.spyOn(fsMock, 'readFileSync').mockImplementation((filePath: any) => {
-  if (filePath.endsWith('wallet/index.json')) {
+  const walletIndexReg = new RegExp(`${sep}test${sep}wallet${sep}index.json`)
+  if (walletIndexReg.test(filePath)) {
     return JSON.stringify(mockWallet.walletIndex)
   }
   const keystorePathReg = new RegExp(`${sep}test${sep}wallet`)
   if (keystorePathReg.test(filePath)) {
     return JSON.stringify(mockWallet.keystores[0])
   }
-  if (filePath.endsWith('setting/index.json')) {
+  const settingIndexPathReg = new RegExp(`${sep}test${sep}setting${sep}index.json`)
+  if (settingIndexPathReg.test(filePath)) {
     return JSON.stringify(mockSetting)
+  }
+  const settingMetaReg = new RegExp(`${sep}test${sep}setting${sep}meta.json`)
+  if (settingMetaReg.test(filePath)) {
+    return JSON.stringify(mockSettingMeta)
   }
   const authPathReg = new RegExp(`${sep}test${sep}auth`)
   if (authPathReg.test(filePath)) {
     return JSON.stringify(mockAuth)
-  }
-  if (filePath.endsWith('setting/meta.json')) {
-    return JSON.stringify(mockSettingMeta)
   }
   const txPathReg = new RegExp(`${sep}test${sep}tx${sep}`)
   if (txPathReg.test(filePath)) {
